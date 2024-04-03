3271f3801a92752123dfc0539d37f4560bbcec6c
Bastian Åström <ba222ji@student.lnu.se>
2024-04-03 16:08:44 +0200

    Updating naming in entities

--- src/main/java/com/sandstrom/entities/Staff.java
+++ src/main/java/com/sandstrom/entities/Staff.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Objects;
 
 @Entity
-@Table(name = "staff", schema = "sakila", catalog = "")
+@Table(name = "staff", schema = "sakila")
 public class Staff {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
