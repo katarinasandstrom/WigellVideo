c9d69a69ceb27d9c644a58cb23735838c734b9f8
shakurqasimi <153449661+shakurqasimi@users.noreply.github.com>
2024-04-04 14:55:42 +0200

    raderat interfaces

--- src/main/java/com/sandstrom/crudOperations/CrudOfCityInterface.java
+++
@@ -1,9 +0,0 @@
-package com.sandstrom.crudOperations;
-
-public interface CrudOfCityInterface {
-    void getAllCities();
-    void getCityById();
-    void saveCity();
-    void deleteCity();
-    void updateCity();
-}
