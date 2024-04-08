375ae2cb69a3c618ca39f79ae63e22d46fe03f5b
Bastian Åström <ba222ji@student.lnu.se>
2024-04-08 20:39:51 +0200

    Commit commit customer

--- src/main/java/com/sandstrom/entities/Country.java
+++ src/main/java/com/sandstrom/entities/Country.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-//forpush
+
 @NamedNativeQuery(name = "Country.pk", query = "SELECT c.country_id from country c WHERE c.country =:country", resultClass = Short.class)
 @Entity
 @Table(name = "country", schema = "sakila")
